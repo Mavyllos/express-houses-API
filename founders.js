87818774d7187ed65e5252037a689f0b499ed2d3
danielbutts <daniel.butts@gmail.com>
2017-03-30 10:11:02 -0700

    adds house_id to founders dataset

--- founders.js
+++ founders.js
@@ -4,27 +4,31 @@ module.exports=[
   last_name: 'Lovelace',
   image_url: 'https://www.biography.com/.image/c_fill,cs_srgb,dpr_1.0,g_face,h_300,q_80,w_300/MTE4MDAzNDEwODQwOTQ2MTkw/ada-lovelace-20825279-1-402.jpg',
   date_of_birth: '12-10-1815',
-  id: 1
+  id: 1,
+  house_id: 4
 },
 {
   first_name: 'Barbara',
   last_name: 'Liskov',
   image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Barbara_Liskov_MIT_computer_scientist_2010.jpg/440px-Barbara_Liskov_MIT_computer_scientist_2010.jpg',
   date_of_birth: '11-07-1939',
-  id: 2
+  id: 2,
+  house_id: 1
 },
 {
   first_name: 'Alan',
   last_name: 'Thicke',
   image_url: 'https://en.wikipedia.org/wiki/File:Alan_Thicke_2010.jpg',
   date_of_birth: '03-01-1947',
-  id: 3
+  id: 3,
+  house_id: 3
 },
 {
   first_name: 'Clarence',
   last_name: 'Ellis',
   image_url: 'http://www.math.buffalo.edu/mad/PIX2/ellis.clarencea.gif',
   date_of_birth: '05-11-1943',
-  id: 4
+  id: 4,
+  house_id: 2
 }
 ]

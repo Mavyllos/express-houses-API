8b21f4ee04a9df466b2960349ec07d9bc7d0bb32
danielbutts <daniel.butts@gmail.com>
2017-03-30 10:07:54 -0700

    adds house data and routes to index, show and delete houses.

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express')
 let founders = require('./founders')
+let houses = require('./houses')
 
 let app = express()
 let port = process.env.PORT || 3000;
+app.listen(port, listen);
 
 app.get('/founders', indexFounders);
 app.get('/founders/:id', showFounder);
 app.delete('/founders/:id', deleteFounder);
-app.listen(port, listen);
+
+app.get('/houses', indexHouses);
+app.get('/houses/:id', showHouse);
+app.delete('/houses/:id', deleteHouse);
 
 function listen(){
   console.log(`listening on ${port} yoooo`)
@@ -18,7 +23,7 @@ function indexFounders(req, res){
 }
 
 function showFounder(req, res){
-  for(let f = 0; f > founders.length; f++){
+  for(let f = 0; f < founders.length; f++){
     if(founders[f].id == req.params.id){
       res.status(200)
       res.json(founders[f])
@@ -30,7 +35,7 @@ function showFounder(req, res){
 }
 
 function deleteFounder(req, res){
-  for(let f = 0; f > founders.length; f++){
+  for(let f = 0; f < founders.length; f++){
     if(founders[f].id == req.params.id){
       founders.splice(f, 1);
       res.status(200)
@@ -40,4 +45,35 @@ function deleteFounder(req, res){
   }
   res.status(400)
   res.json({message: `Founder '${req.params.id}' not found`})
+}
+
+function indexHouses(req, res){
+  res.json(houses)
+}
+
+function showHouse(req, res){
+  console.log(houses.length);
+  for (let h = 0; h < houses.length; h++) {
+    console.log(`YO! ${houses[h].id} ${req.params.id} ${houses[h].id == req.params.id}`);
+    if(houses[h].id == req.params.id){
+      res.status(200)
+      res.json(houses[h])
+      return
+    }
+  }
+  res.status(400)
+  res.json({message: `House '${req.params.id}' not found`})
+}
+
+function deleteHouse(req, res){
+  for(let h = 0; h < founders.length; h++){
+    if(founders[h].id == req.params.id){
+      founders.splice(h, 1);
+      res.status(200)
+      res.json({message: `House '${req.params.id}' successfully deleted`})
+      return
+    }
+  }
+  res.status(400)
+  res.json({message: `House '${req.params.id}' not found`})
 }
---
+++ houses.js
@@ -0,0 +1,30 @@
+module.exports =  [
+  {
+    name:'Liskov',
+    color:'Onyx',
+    symbol:'\{\}',
+    mascot:'Chimera',
+    id:1
+  },
+  {
+    name:'Ellis',
+    color:'Steel Teal',
+    symbol:'lambda',
+    mascot:'Narwhal',
+    id:2
+  },
+  {
+    name:'Turing',
+    color:'Sunglow',
+    symbol:'Tree',
+    mascot:'Yellowjacket',
+    id:3
+  },
+  {
+    name:'Lovelace',
+    color:'Mango Tango',
+    symbol:'Pi',
+    mascot:'Llama',
+    id:4
+  }
+]
